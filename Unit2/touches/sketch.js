d919bf1572eaa7ab068398eb8dac62d17f61e3af
elle-art <94562354+elle-art@users.noreply.github.com>
2022-10-08 16:44:21 -0500

    touches

--- Unit2/touches/sketch.js
+++ Unit2/touches/sketch.js
@@ -5,6 +5,7 @@ function setup() {
   i1 = loadImage("assets/1.jfif");
   i2 = loadImage("assets/2.jpg");
   i3 = loadImage("assets/3.jpg");
+  rectMode(CENTER);
 }
 
 function draw() {
@@ -18,17 +19,17 @@ function draw() {
       break ;
       
       case 1: 
-       text("one is the loneliest number", 5, 22) ; 
+       text("one is the loneliest number", width/2, 22) ; 
        image(i1, width/2, height/2, 200, 200) ;
       break ;
       
       case 2:
-      text("two can be as bad as one, it's the loneliest number since the number one", 5, 22) ; 
+      text("two can be as bad as one, \nit's the loneliest number since the number one", width/2, 22) ; 
       image(i2, width/2, height/2, 200, 200) ;
       break ;
       
       case 3:
-     text("three's a crowd though", 5, 22) ; 
+     text("three's a crowd though", width/2, 22) ; 
      image(i3, width/2, height/2, 200, 200) ;
       break ;
     
